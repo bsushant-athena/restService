efa8b100418202dccb865c4c62c4fbc34852cda9
Sushant Borse <sushant91265@gmail.com>
2020-03-08 20:37:52 +0530

    post req

--- src/main/java/com/example/restService/GreetingController.java
+++ src/main/java/com/example/restService/GreetingController.java
@@ -14,4 +14,13 @@ public class GreetingController {
 	public com.example.restService.Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new com.example.restService.Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
+
+	@RequestMapping(value = "/test", method = RequestMethod.POST)
+	public PostResponse Test(@RequestBody PostRequest inputPayload) {
+		PostResponse response = new PostResponse();
+		response.setId(inputPayload.getId()*100);
+		response.setMessage("Hello " + inputPayload.getName());
+		response.setExtra("Some text");
+		return response;
+	}
 }
